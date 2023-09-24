5c82ad063c13523b0c62f72ee236bf0938f2422b
Lautaro Morales <lautamorales17@gmail.com>
2023-09-24 14:14:43 -0300

    Getters y setters en DTOReport

--- Trabajo Entregable 2/src/main/resources/tp2/dto/DTOReport.java
+++ Trabajo Entregable 2/src/main/resources/tp2/dto/DTOReport.java
@@ -12,6 +12,38 @@ public class DTOReport {
 		this.year = year;
 		this.enrolled_qty = cant_inscriptos;	
 		this.graduated_qty = cant_egresos;
+	}	
+
+	public String getCareer_name() {
+		return career_name;
+	}
+
+	public void setCareer_name(String career_name) {
+		this.career_name = career_name;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public Double getEnrolled_qty() {
+		return enrolled_qty;
+	}
+
+	public void setEnrolled_qty(Double enrolled_qty) {
+		this.enrolled_qty = enrolled_qty;
+	}
+
+	public Double getGraduated_qty() {
+		return graduated_qty;
+	}
+
+	public void setGraduated_qty(Double graduated_qty) {
+		this.graduated_qty = graduated_qty;
 	}
 
 	@Override
