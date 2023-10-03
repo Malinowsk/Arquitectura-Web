3c46c9e773a45f36f530258bc5407ffc9d82be5d
Lautaro Morales <lautamorales17@gmail.com>
2023-10-03 19:03:14 -0300

    Se agrega proyecto default de Spring en carpeta entrega 3

---
+++ Trabajo Entregable 3/src/main/java/com/example/trabajoentregable3/TrabajoEntregable3Application.java
@@ -0,0 +1,15 @@
+package com.example.trabajoentregable3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoEntregable3Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TrabajoEntregable3Application.class, args);
+        System.out.println("Hola mundo");
+    }
+
+}
---
+++ Trabajo Entregable 3/src/test/java/com/example/trabajoentregable3/TrabajoEntregable3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabajoentregable3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoEntregable3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
