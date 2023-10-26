bb593ea07ba0dc5bc2ca516b5f2fb5bd3f5c1e3d
Malinowsk <juanrago2015@gmail.com>
2023-10-25 22:56:35 -0300

    Agrego carpetas y entidades a los microservicios de monopatin y estacion

--- TP Especial/microservices_scooters/src/main/java/com/example/microservices_scooters/entity/Ride.java
+++ TP Especial/microservices_scooters/src/main/java/com/example/microservices_scooters/entity/Ride.java
@@ -1,11 +1,10 @@
 package com.example.microservices_scooters.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
 
 @Entity
 @Data  // genera automáticamente los métodos getter, setter, toString(), equals(), y hashCode()
@@ -15,6 +14,17 @@ public class Ride {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long  id;
-
+    @Column
+    private long  id_user;
+    @Column
+    private long  id_account;
+    @Column
+    private Timestamp initiated;
+    @Column
+    private Timestamp finalized;
+    @Column
+    private float kilometers_traveled;
+    @Column
+    private float total_price;
 
 }
