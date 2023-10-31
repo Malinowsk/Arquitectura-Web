355a6a666b16a4353f1bd7973766a737491420c2
Lautaro Morales <lautamorales17@gmail.com>
2023-10-30 21:30:00 -0300

    Archivos varios como pom y config de maven

---
+++ TP Especial/microservices_admin_maintenance/src/test/java/com/example/microservices_admin_maintenance/MicroservicesAdminMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices_admin_maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesAdminMaintenanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
