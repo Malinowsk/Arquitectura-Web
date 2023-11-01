3c5eea3bd772f1f5eb5b9d04a98fa90d63cd7013
Lautaro Morales <lautamorales17@gmail.com>
2023-10-31 22:44:05 -0300

    Cambio en AdminService para devolver solamente el body del exchange

--- TP Especial/microservices_admin_maintenance/src/main/java/com/example/microservices_admin_maintenance/service/AdminService.java
+++ TP Especial/microservices_admin_maintenance/src/main/java/com/example/microservices_admin_maintenance/service/AdminService.java
@@ -1,14 +1,11 @@
 package com.example.microservices_admin_maintenance.service;
 
-import com.example.microservices_admin_maintenance.dto.DTORequestScooter;
 import com.example.microservices_admin_maintenance.dto.DTORequestScooterModel;
-import com.example.microservices_admin_maintenance.dto.DTOResponseScooter;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,13 +14,12 @@ import org.springframework.web.client.RestTemplate;
 public class AdminService {
 
     @Transactional
-    public ResponseEntity<String> createScooter(DTORequestScooterModel scooterModel) {
+    public String createScooter(DTORequestScooterModel scooterModel) {
         String url = "http://localhost:8003/api/monopatines";
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         HttpEntity<DTORequestScooterModel> requestEntity = new HttpEntity<>(scooterModel, headers);
-        System.out.print(scooterModel);
-        return restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        return restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class).getBody();
     }
 
 }
