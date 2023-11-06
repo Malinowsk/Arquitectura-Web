c51bd89059242926e1695778c9c8d6945f5e429c
Malinowsk <juanrago2015@gmail.com>
2023-11-06 10:25:52 -0300

    modificacion en el servicio que devuelve la cant de monopatines en uso vs en mantenimiento

--- TP Especial/microservices_scooters/src/main/java/com/example/microservices_scooters/repository/ScooterRepository.java
+++ TP Especial/microservices_scooters/src/main/java/com/example/microservices_scooters/repository/ScooterRepository.java
@@ -18,7 +18,6 @@ public interface ScooterRepository extends JpaRepository<Scooter,Long> {
     @Query("SELECT s FROM Scooter s JOIN Ride r ON (r.scooter.id = s.id) WHERE s.numberOfTrips >= :cant and YEAR(r.initiated) = :anio GROUP BY s.id")
     List<Scooter> getBySearch(int cant, int anio);
     @Query("SELECT new com.example.microservices_scooters.dto.DTORespondeStatusQualityScooter( SUM(CASE WHEN s.state = :en_uso THEN 1 ELSE 0 END), SUM(CASE WHEN s.state = :mantenimiento THEN 1 ELSE 0 END)) FROM Scooter s")
-    //@Query("SELECT s.state, count(*) FROM Scooter s WHERE s.state = :en_uso or s.state = :mantenimiento group by s.state")
     DTORespondeStatusQualityScooter getQuantityBasedOnStatus(String en_uso, String mantenimiento);
     @Query("SELECT s FROM Scooter s WHERE s.location.latitud-5.0 <= :latitud and s.location.latitud+5.0 >= :latitud and s.location.longitud-5.0 <= :longitud and s.location.longitud+5.0 >= :longitud")
     List<Scooter> getScootersSurroundings(double longitud, double latitud);
