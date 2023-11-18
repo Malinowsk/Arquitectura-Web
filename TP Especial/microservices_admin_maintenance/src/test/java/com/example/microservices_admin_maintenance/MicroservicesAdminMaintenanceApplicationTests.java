8717552c6aa65e00a214b0158c8de8a03ea3a870
Lautaro Morales <lautamorales17@gmail.com>
2023-11-18 00:56:28 -0300

    Se agrega un test para encontrar por ID

--- TP Especial/microservices_admin_maintenance/src/test/java/com/example/microservices_admin_maintenance/MicroservicesAdminMaintenanceApplicationTests.java
+++ TP Especial/microservices_admin_maintenance/src/test/java/com/example/microservices_admin_maintenance/MicroservicesAdminMaintenanceApplicationTests.java
@@ -2,32 +2,29 @@ package com.example.microservices_admin_maintenance;
 
 import com.example.microservices_admin_maintenance.controller.AdministrationController;
 import com.example.microservices_admin_maintenance.controller.FareController;
-import com.example.microservices_admin_maintenance.dto.DTOFareResponse;
-import com.example.microservices_admin_maintenance.service.FareService;
-import io.swagger.v3.core.util.Json;
-import org.apache.coyote.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.*;
 
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -42,64 +39,42 @@ class MicroservicesAdminMaintenanceApplicationTests {
 
     @MockBean
     private AdministrationController adminController;
-/*
-    @Test
-    @WithMockUser(authorities = {"ADMIN"})
-    public void getById() throws Exception {
 
-        //Raro esto pero no encuentro mucha info en como testear un metodo que devuelve ResponseEntity<?>, testee un par de formas aca
-        //pero tiran error
+
+
+    @Test
+    public void getFareById() throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Connection", "keep-alive");
+        headers.add("Accept-Encoding", "gzip, deflate, br");
+        headers.add("Accept", "/");
+
         ResponseEntity testDTOResponse = new ResponseEntity(
                 "{\"id\":\"654123\",\"name\":\"comun\",\"cost_per_min\":150.0,\"extended_pause_cost\":200.0}",
-                new HttpHeaders(),
+                headers,
                 HttpStatus.OK
         );
 
-        //Si se borran esta y la parte anterior, da como fallido el test
         Mockito.when(
-                fareController.findById(Mockito.anyString(),null)
+                fareController.findById(Mockito.anyString(), any(HttpHeaders.class))
         ).thenReturn(testDTOResponse);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/tarifas/6546a76d8703784d0d60d935")
-                .accept(MediaType.APPLICATION_JSON);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/tarifas/1")
+                .accept(MediaType.APPLICATION_JSON).headers(headers);
 
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
-
-        String expected = "{\"id\":\"654123\",\"name\":\"comun\",\"cost_per_min\":150.0,\"extended_pause_cost\":200.0}";
-
-        System.out.println(expected);
-
-        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.id", is("654123")))
+                .andReturn();
     }
 
     @Test
     public void getAllFares() throws Exception {
-        DTOFareResponse testFare1 = new DTOFareResponse("123", "test1", 20.0, 30.0);
-        DTOFareResponse testFare2 = new DTOFareResponse("456", "test2", 40.0, 60.0);
 
-        List<DTOFareResponse> testList = new ArrayList<DTOFareResponse>();
-        testList.add(testFare1);
-        testList.add(testFare2);
-
-        Mockito.when(
-                fareController.findAll()
-        ).thenReturn(testList);
-
-        RequestBuilder req = MockMvcRequestBuilders.get("/api/tarifas").accept(MediaType.APPLICATION_JSON);
-
-        List<DTOFareResponse> resultList = fareController.findAll();
-
-        mockMvc.perform(req)
-            .andExpect(status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.size()")
-                    .value(testList.size()))
-                .andDo(print());
-
-        //Revisar, el assert es necesario, pero esta bien planteado?
-        JSONAssert.assertEquals(resultList.toString(), testList.toString(), false);
-    }*/
+    }
 
     @Test
     void contextLoads() throws Exception {
