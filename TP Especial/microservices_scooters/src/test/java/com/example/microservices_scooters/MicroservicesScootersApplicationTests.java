dc3133861cd753ca50d2e2df34d62890e90b8aae
Malinowsk <juanrago2015@gmail.com>
2023-11-18 03:16:56 -0300

    Agrego test de accounts y scooters

--- TP Especial/microservices_scooters/src/test/java/com/example/microservices_scooters/MicroservicesScootersApplicationTests.java
+++ TP Especial/microservices_scooters/src/test/java/com/example/microservices_scooters/MicroservicesScootersApplicationTests.java
@@ -1,10 +1,88 @@
 package com.example.microservices_scooters;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.example.microservices_scooters.dto.DTOResponseScooter;
+import com.example.microservices_scooters.entity.Scooter;
+import com.example.microservices_scooters.service.ScooterService;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class MicroservicesScootersApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ScooterService scooterService;
+
+    @Test
+    void testFindAll() throws Exception {
+        // Datos de ejemplo para la respuesta del servicio
+        List<DTOResponseScooter> scooters = new ArrayList<>();
+
+        Scooter scooter1;
+        scooter1 = new Scooter("Toyota");
+        scooter1.setKmsTraveled(1000.88);
+        scooter1.setState("en_mantenimiento");
+        scooter1.setTotalUsageTime(500L);
+        scooter1.setPausedTime(50L);
+
+        Scooter scooter2;
+        scooter2 = new Scooter("Honda");
+        scooter2.setKmsTraveled(1020.88);
+        scooter2.setState("en_mantenimiento");
+
+        DTOResponseScooter scooter3 = new DTOResponseScooter(scooter1);
+        DTOResponseScooter scooter4 = new DTOResponseScooter(scooter2);
+        scooters.add(scooter3);
+        scooters.add(scooter4);
+
+        // Configuración del comportamiento del servicio mock
+        when(scooterService.findAll(any(HttpHeaders.class))).thenReturn(scooters);
+
+        // Construir la solicitud
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/monopatines")
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .header("Otro-Encabezado", "valor-otro-encabezado");
+
+        // Realizar la solicitud y realizar afirmaciones
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.size()", is(scooters.size())))
+                // .andExpect(jsonPath("$[0].id", is("1")))
+                .andExpect(jsonPath("$[0].state", is("en_mantenimiento")))
+                .andExpect(jsonPath("$[0].kmsTraveled", is(1000.88)))
+                .andExpect(jsonPath("$[0].totalUsageTime", is(500)))
+                .andExpect(jsonPath("$[0].pausedTime", is(50)))
+                .andExpect(jsonPath("$[0].model", is("Toyota")))
+                //.andExpect(jsonPath("$[1].id", is("2")))
+                .andExpect(jsonPath("$[1].state", is("en_mantenimiento")))
+                .andExpect(jsonPath("$[1].kmsTraveled", is(1020.88)))
+                .andExpect(jsonPath("$[1].model", is("Honda")));
+    }
+
 
     @Test
     void contextLoads() {
--- TP Especial/microservices_users/src/test/java/com/example/microservices_users/MicroservicesUsersApplicationTests.java
+++ TP Especial/microservices_users/src/test/java/com/example/microservices_users/MicroservicesUsersApplicationTests.java
@@ -1,10 +1,86 @@
 package com.example.microservices_users;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.microservices_users.dto.DTORequestAccount;
+import com.example.microservices_users.dto.DTOResponseAccount;
+import com.example.microservices_users.service.AccountService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class MicroservicesUsersApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AccountService accountService;
+
+    @Test
+    void testFindAll() throws Exception {
+        List<DTOResponseAccount> accounts = Collections.singletonList(new DTOResponseAccount());
+
+        when(accountService.findAll()).thenReturn(accounts);
+
+        mockMvc.perform(get("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcEBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTixST0xFX01BSU5URU5BTkNFLFJPTEVfVVNFUiIsImV4cCI6MTcwMDMxNjM0Nn0.ZDvDEdCGWd2PsWiLuYrQFVDfP7ybJd1Do2gLOZX3aEUw_EQbhstEtJK0KPvym2T_G3eshYqV7UJH_OKtpd37aQ"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.size()").value(accounts.size()));
+    }
+
+    @Test
+    void testGetAccountByID() throws Exception {
+        long accountId = 1L;
+        DTOResponseAccount account = new DTOResponseAccount();
+
+        when(accountService.findById(accountId)).thenReturn(account);
+
+        mockMvc.perform(get("/api/accounts/{id}", accountId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcEBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTixST0xFX01BSU5URU5BTkNFLFJPTEVfVVNFUiIsImV4cCI6MTcwMDMxNjM0Nn0.ZDvDEdCGWd2PsWiLuYrQFVDfP7ybJd1Do2gLOZX3aEUw_EQbhstEtJK0KPvym2T_G3eshYqV7UJH_OKtpd37aQ"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.active").value(false));
+    }
+
+    @Test
+    void testSave() throws Exception {
+        DTORequestAccount request = new DTORequestAccount();
+        DTOResponseAccount savedAccount = new DTOResponseAccount();
+
+        when(accountService.save(request)).thenReturn(savedAccount);
+
+        mockMvc.perform(post("/api/accounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcEBnbWFpbC5jb20iLCJhdXRoIjoiUk9MRV9BRE1JTixST0xFX01BSU5URU5BTkNFLFJPTEVfVVNFUiIsImV4cCI6MTcwMDMxNjM0Nn0.ZDvDEdCGWd2PsWiLuYrQFVDfP7ybJd1Do2gLOZX3aEUw_EQbhstEtJK0KPvym2T_G3eshYqV7UJH_OKtpd37aQ"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(savedAccount.getId()));
+    }
 
     @Test
     void contextLoads() {
